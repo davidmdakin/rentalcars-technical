c6723e80cf63bb5190af7def69ac1e682cdf7eca
davidmdakin <davidmdakin@gmail.com>
2016-05-23 18:42:14 +0100

    ADD: ex2 output to car class

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -41,8 +41,95 @@ public class Car {
 		return this.rating;
 	}
 	
+	String getSipp1()
+	{
+		char sipp = this.getSipp().charAt(0);
+		switch (sipp)
+		{
+    	case 'M':
+    		return "Mini";
+		case 'E':
+    		return "Economy";
+		case 'C':
+    		return "Compact";
+		case 'I':
+    		return "Intermediate";
+		case 'S':
+    		return "Standard";
+		case 'F':
+    		return "Full size";
+		case 'P':
+    		return "Premium";
+		case 'L':
+    		return "Luxury";
+		case 'X':
+    		return "Special";
+		default:
+			return "Unknown";
+		}
+	}
+	
+	String getSipp2()
+	{
+		char sipp = this.getSipp().charAt(1);
+		switch (sipp)
+		{
+	    	case 'B':
+	    		return "2 doors";
+	    	case 'C':
+	    		return "4 doors";
+	    	case 'D':
+	    		return "5 doors";
+	    	case 'W':
+	    		return "Estate";
+	    	case 'T':
+	    		return "Convertible";
+	    	case 'F':
+	    		return "SUV";
+	    	case 'P':
+	    		return "Pick up";
+	    	case 'V':
+	    		return "Passenger Van";
+			default:
+				return "Unknown";
+    	}
+	}
+	
+	String getSipp3()
+	{
+		char sipp = this.getSipp().charAt(2);
+		switch (sipp)
+		{
+    	case 'M':
+    		return "Manual";
+    	case 'A':
+    		return "Automatic";
+		default:
+			return "Unknown";
+    	}
+	}
+	
+	String getSipp4()
+	{
+		char sipp = this.getSipp().charAt(3);
+		switch (sipp)
+		{
+    	case 'N':
+    		return "Petrol - No AC";
+    	case 'R':
+    		return "Petrol - AC";
+		default:
+			return "Unknown - Unknown";
+    	}
+	}
+	
 	public String getEx1Output()
 	{
 		return this.getName() + " - " + this.getPrice();
+	}
+	
+	public String getEx2Output()
+	{
+		return this.getName() + " - " + this.getSipp() + " - " + this.getSipp1() + " - " + this.getSipp2() + " - " + this.getSipp3() + " - " + this.getSipp4();
 	}
 }
