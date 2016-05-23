1767f4e782cfb3dc2b52d12743f864a81c0de4bd
davidmdakin <davidmdakin@gmail.com>
2016-05-23 16:42:02 +0100

    ADD: Ex1 output, static methods for tests to pass

---
+++ src/main/java/Cars.java
@@ -0,0 +1,64 @@
+package main.java;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class Cars {
+	public static void main(String[] args)
+	{
+		String input = "";
+		try {
+			input = readFile("C:/vehicles.json");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		if (input.length() > 0)
+		{
+			ArrayList<Car> carList = JsonStringToCarList(input);
+			carList = SortByPriceAsc(carList);
+			for (Car car : carList)
+			{
+				System.out.println(car.getEx1Output());
+			}
+		}
+	}
+	
+	public static String readFile(String path) throws IOException 
+	{
+	  byte[] encoded = Files.readAllBytes(Paths.get(path));
+	  return new String(encoded);
+	}
+	
+	public static ArrayList<Car> JsonStringToCarList(String jsonString)
+	{
+		JsonObject jsonObject = new JsonParser().parse(jsonString).getAsJsonObject();
+		JsonArray jsa = jsonObject.get("Search").getAsJsonObject().get("VehicleList").getAsJsonArray();
+		
+		Gson gson = new Gson();
+
+	    Car[] navigationArray = gson.fromJson(jsa, Car[].class);
+	    ArrayList<Car> carList = new ArrayList<Car>();
+	    for (Car car : navigationArray)
+	    {
+	    	carList.add(car);
+	    }
+	    return carList;
+	}
+	
+	public static ArrayList<Car> SortByPriceAsc(ArrayList<Car> carList)
+	{
+		Collections.sort(carList, (Car p1, Car p2) -> Float.compare(p1.getPrice(), p2.getPrice()));
+		return carList;
+	}
+
+}
