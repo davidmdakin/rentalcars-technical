0f2e4ef53ea10f30c9ebf83e7aec53ad8337ca7c
davidmdakin <davidmdakin@gmail.com>
2016-05-23 16:27:02 +0100

    ADD: Test for sorting of cars in first ex.

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,15 +1,38 @@
 package test.java;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 import main.java.Car;
+import main.java.Cars;
 
 public class CarTest {
 	@Test
-	public final void CarToEx1Output() {
+	public final void CarToEx1Output()
+	{
 		Car car = new Car("CDMR", "Ford Focus", 157.85f, "Hertz", 8.9f);
 		String ex1Output = car.getEx1Output();
 		assertEquals(ex1Output, "Ford Focus - 157.85");
 	}
 	
+	@Test
+	public final void CarListSortedByPriceAsc()
+	{
+		String json = "";
+		try {
+			json = Cars.readFile("C:/vehicles.json");
+		}
+		catch (Exception e)
+		{
+			fail("Exception " + e);
+		}
+		ArrayList<Car> carList = Cars.JsonStringToCarList(json);
+		carList = Cars.SortByPriceAsc(carList);
+		for (int i = 0; i < carList.size() - 1; i++)
+		{
+			assertTrue(carList.get(i).getPrice() < carList.get(i+1).getPrice());
+		}
+	}
 }
